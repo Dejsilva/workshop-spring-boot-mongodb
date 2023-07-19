617f978db5420817ac6159d66086ab50e473aea0
Deivide <dejs_silva@hotmail.com>
2023-07-19 12:22:41 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/deividesilva/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.deividesilva.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.deividesilva.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
